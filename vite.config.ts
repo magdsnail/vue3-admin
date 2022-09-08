6b3db218814cbdaf085fa9eadd2e56dffc7d85d8
jason.wang <wangkjim@gmail.com>
2022-09-08 10:58:29 +0800

    ...

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'node:path'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  resolve: {
+    alias: [{
+      find: '@',
+      replacement: resolve(__dirname, 'src')
+    }]
+  }
 })
+
