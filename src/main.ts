4b9877fe30f01f2848403ea936cee5de93e7584a
jason.wang <wangkjim@gmail.com>
2022-09-08 10:48:23 +0800

    ..

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import './style.css'
+import 'element-plus/dist/index.css'
+
+import App from './App.vue'
+import ElementPlus from 'element-plus'
+import { createApp } from 'vue'
+
+const app = createApp(App)
+
+app.use(ElementPlus)
+app.mount('#app')
